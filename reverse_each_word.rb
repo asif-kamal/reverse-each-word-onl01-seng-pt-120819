11b916a274d5a08d12060cd490b2316e0594b5f8
Asif Kamal <asif_kamal@hotmail.com>
2019-12-20 01:08:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require "pry"
 def reverse_each_word(string)
   array = []
+  new_array = []
   array = string.split
   array.collect do |word|
-    new_array = []
     new_array << word.reverse
   end
    new_array.join(" ")
