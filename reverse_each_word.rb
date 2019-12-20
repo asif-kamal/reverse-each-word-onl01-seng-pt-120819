9773d171f131ff7cedf43c3bf113365593b6693e
Asif Kamal <asif_kamal@hotmail.com>
2019-12-20 01:07:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(string)
   array = []
   array = string.split
   array.collect do |word|
-    word.reverse
+    new_array << word.reverse
   end
-   array.join(" ")
+   new_array.join(" ")
 end
   
